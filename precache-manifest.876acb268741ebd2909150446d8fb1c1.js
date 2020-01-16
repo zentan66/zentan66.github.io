4cf4f144ae04971b9a55170039c8198395309f62
zentan66 <1041944816@qq.com>
2020-01-16 21:47:30 +0800

    添加播放器

---
+++ precache-manifest.876acb268741ebd2909150446d8fb1c1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7b52ab302c59971e5fb74344289c26",
+    "url": "/muxue-music/index.html"
+  },
+  {
+    "revision": "fb027db473e9a3a13ffc",
+    "url": "/muxue-music/static/css/main.d522486b.chunk.css"
+  },
+  {
+    "revision": "75be17bfcfb43dfcf07c",
+    "url": "/muxue-music/static/js/2.4f8348b7.chunk.js"
+  },
+  {
+    "revision": "ba8e0976f8fc6adfb602e33ba7bbd2e9",
+    "url": "/muxue-music/static/js/2.4f8348b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb027db473e9a3a13ffc",
+    "url": "/muxue-music/static/js/main.4ef6c663.chunk.js"
+  },
+  {
+    "revision": "9271e069fb452ce53197",
+    "url": "/muxue-music/static/js/runtime-main.f7adfdb3.js"
+  },
+  {
+    "revision": "c8b6513aab94f8345122185586351b23",
+    "url": "/muxue-music/static/media/avatar.c8b6513a.png"
+  },
+  {
+    "revision": "a44c963756537a3b5d5bc257ec68dc1f",
+    "url": "/muxue-music/static/media/bg.a44c9637.jpeg"
+  },
+  {
+    "revision": "52af51cc8bef242df15e715b0b4481df",
+    "url": "/muxue-music/static/media/iconfont.52af51cc.eot"
+  },
+  {
+    "revision": "93701aed79f3831deb7394ada8c3d710",
+    "url": "/muxue-music/static/media/iconfont.93701aed.ttf"
+  },
+  {
+    "revision": "99d916a7b991c8cf5a6da561173ce25f",
+    "url": "/muxue-music/static/media/iconfont.99d916a7.svg"
+  },
+  {
+    "revision": "b495323616fd98803078bdfe983f5c5e",
+    "url": "/muxue-music/static/media/iconfont.b4953236.woff"
+  },
+  {
+    "revision": "da7ff67bb105b9eca30ec9f00afe8d14",
+    "url": "/muxue-music/static/media/image-bg.da7ff67b.jpeg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/muxue-music/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "c9dd9d8ee22227a6832ed5eea3fc7904",
+    "url": "/muxue-music/static/media/rank.c9dd9d8e.jpeg"
+  },
+  {
+    "revision": "abf0f817349caa55389a890edfffa21e",
+    "url": "/muxue-music/static/media/句号-G.E.M.邓紫棋.abf0f817.mp3"
+  }
+]);
